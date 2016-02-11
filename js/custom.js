c553580924705775d38ee854f5351474bce5f5f7
Alan Rhode <alanmichaelrhode@gmail.com>
2016-02-11 10:08:24 -0500

    my website

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('.ruby').css('width', '90%');
+  $('.html').css('width', '90%');
+  $('.javascript').css('width', '90%');
+  $('.collaborative').css('width', '90%');
+  $('.activerecord').css('width', '90%');
+  $('.sinatra').css('width', '80%');
+  $('.bootstrap').css('width', '80%');
+  $('.startup').css('width', '95%');
+  $('.finance').css('width', '95%');
+  $('.marketing').css('width', '95%');
+  $('.management').css('width', '95%');
+  $('.investments').css('width', '95%');
+  $('.financial').css('width', '95%');
+  $('.employee').css('width', '80%');
+});
